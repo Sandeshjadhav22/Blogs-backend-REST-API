db7980b30755c382cc88b53e6138939119267368
sandesh jadhav <sandeshjadhav2002@gmail.com>
2023-09-24 17:08:03 +0530

    Env file added

--- app.js
+++ app.js
@@ -2,17 +2,15 @@ import express from "express";
 import mongoose from "mongoose";
 import router from "./routes/user-routes";
 import blogRouter from "./routes/blog-routes";
+import * as dotenv from 'dotenv';
+dotenv.config();
 
-
-
-const app = express(); 
+const app = express();
 app.use(express.json());
-app.use("/api/user",router)
-app.use("/api/blog",blogRouter);
+app.use("/api/user", router);
+app.use("/api/blog", blogRouter);
 mongoose
-  .connect(
-    "mongodb+srv://admin:sandy2002@cluster0.gjpuqm7.mongodb.net/Blog?retryWrites=true&w=majority"
-  )
+  .connect(process.env.URI)
   .then(() => app.listen(5000))
   .then(() => console.log("Connected To Database & Listning to port of 5000"))
   .catch((err) => console.log(err));
